b773e041ae5a922b22b850aac39a089af8acefb0
Rodrigo Faria <rodrigomfaria@gmail.com>
2025-01-06 21:42:24 -0300

    feat: django templates

--- recipes/views.py
+++ recipes/views.py
@@ -5,6 +5,6 @@ def home(request):
     return render(request, 'recipes/pages/home.html')
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'name': 'rmf',
     })
