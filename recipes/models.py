6c598a601c36c4a60dac3944a27c7a669abfd26f
Rodrigo Faria <rodrigomfaria@gmail.com>
2025-05-17 21:28:36 -0300

    feat: add recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -12,9 +12,11 @@ class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
+    preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
-    servings_steps = models.TextField()
+    preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -26,5 +28,8 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
 
 
